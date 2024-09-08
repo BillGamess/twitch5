04204623b41d33c116c7a1402fd6488de53ae488
BillGamess <167017829+BillGamess@users.noreply.github.com>
2024-09-08 22:54:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ twitch_miner = TwitchChannelPointsMiner(
             BET_wiN=Fore.MAGENTA                # Color allowed are: [BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET].
         ),
         telegram=Telegram(                                                          # You can omit or set to None if you don't want to receive updates on Telegram
-            chat_id=,                                                      # Chat ID to send messages @getmyid_bot
-            token="",                          # Telegram API token @BotFather
+            chat_id=123456789,                                                      # Chat ID to send messages @getmyid_bot
+            token="123456789:shfuihreuifheuifhiu34578347",                          # Telegram API token @BotFather
             events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
                     Events.GAIN_FOR_RAID, Events.GAIN_FOR_CLAIM, Events.GAIN_FOR_WATCH, Events.GAIN_FOR_WATCH_STREAK, Events.JOIN_RAID, Events.BONUS_CLAIM],                          # Only these events will be sent to the chat
             disable_notification=True,                                              # Revoke the notification (sound/vibration)
